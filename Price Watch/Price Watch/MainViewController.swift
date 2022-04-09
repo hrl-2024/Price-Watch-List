54c8dfe67cb3abbde5d888abcc1587ad82097894
hrl-2024 <90875886+hrl-2024@users.noreply.github.com>
2022-04-08 21:43:48 -0400

    Initial Push

---
+++ Price Watch/Price Watch/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  Price Watch
+//
+//  Created by Henry Liu on 4/8/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // Outlet
+    @IBOutlet weak var searchBar: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
